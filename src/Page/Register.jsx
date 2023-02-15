c3581ae6fe0090d5c2edbb5abb683b85e9b1a4a4
Aman Kumar <aman_sinha@live.com>
2023-02-15 19:26:20 +0530

    login page is active

--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -5,7 +5,7 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
@@ -67,7 +67,9 @@ const Register = () => {
           <button>SignUp</button>
           {err && <span>Something went Wrong!</span>}
         </form>
-        <p>You already have an account? Login</p>
+        <p>
+          You already have an account?<Link to="/login">Login</Link>
+        </p>
       </div>
     </div>
   );
